bc2bc286ac50fadfd97e75de08bbe6048d97ef24
nevets615 <surj316@yahoo.com>
2019-04-24 17:14:03 -0400

    working on my route components

--- data/helpers/posts-router.js
+++ data/helpers/posts-router.js
@@ -17,4 +17,74 @@ router.get("/", (req, res) => {
       });
   });
 
+ router.get("/:id", (req, res) => {
+    userId = req.params.id;
+    db.findById(userId)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ error: "The user with the specified ID does not exist" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The user information could not be retrieved" });
+      });
+  });
+  router.post("/", (req, res) => {
+    const newPost = req.body;
+    console.log("request body: ", newPost);
+  
+    if (newPost) {
+      db.insert(newPost)
+        .then(posts => {
+          res.status(201).json(posts);
+        })
+        .catch(err => {
+          res
+            .status(400)
+            .json({ errorMessage:  "Please provide title and contents for the post."  });
+        });
+    } else {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database" 
+        });
+    }
+  });
+
+  router.delete("/:id", (req, res) => {
+    const userId = req.params.id;
+    db.posts
+    .remove(postId)
+    .then(deleted => {
+        res.status(404)
+        message: "The post with the specified ID does not exist."
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          message: "The post could not be removed" 
+        });
+      });
+  });
+  
+  router.put("/:id", (req, res) => {
+    const userId = req.params.id;
+    const updateInfo = req.body
+    if (updateInfo.name && updateInfo.bio) {
+      db.update(userId, updateInfo)
+        .then(user => {
+         res.status(200).json(user)
+        })
+        .catch(err => {
+          res.status(500).json({error: err, message: "there was an error updating the user"})
+        })
+      } else {
+        res.status(400).json({message: "please provide a name and bio"})
+      }
+  
+  })
+  
 module.exports = router;
